9944a6ca83b991d373b25d53685508148b9bb1ff
tom <tom@tomdeMac-mini.local>
2026-06-11 12:47:42 +0800

    比赛时间固定为北京时间

--- public/app.js
+++ public/app.js
@@ -46,12 +46,13 @@ function formatKickoff(value) {
   const date = new Date(value);
   if (Number.isNaN(date.getTime())) return value;
   return date.toLocaleString('zh-CN', {
+    timeZone: 'Asia/Shanghai',
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
     hour12: false
-  });
+  }) + ' 北京时间';
 }
 
 function renderMatchSummary() {
